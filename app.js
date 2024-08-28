a7bb597272af9ab1c9d9b3373e6c748d690db70e
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-28 14:27:57 +0200

    try debug CORS

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: "https://gaminretroreact-frontend.vercel.app/", // Votre domaine frontend
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Origin", "Content-Type", "Authorization"],
   })
 );
 

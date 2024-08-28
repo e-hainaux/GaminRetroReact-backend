65e630bcd444e308d16e20aaf4f51b6a46214e3f
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-28 17:01:14 +0200

    change import fetch back to require

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 const cloudinary = require("cloudinary").v2;
 const Game = require("../models/games");
 
